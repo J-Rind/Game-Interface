f2cf64cc42609a472af95ec8cadceba7ca181258
brockherion <brockjosephherion@gmail.com>
2020-12-12 16:51:51 -0600

    Update

---
+++ src/com/company/GameLoader.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class GameLoader {
+    private Board board;
+    private Player whitePlayer;
+    private Player blackPlayer;
+    private ArrayList<Piece> pieces;
+
+    public GameLoader() { }
+
+    public void loadGameFromFile(String path) {
+        try {
+
+        } catch(Exception ex) {
+
+        }
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public Player getWhitePlayer() {
+        return whitePlayer;
+    }
+
+    public Player getBlackPlayer() {
+        return blackPlayer;
+    }
+}
