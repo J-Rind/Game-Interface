165b620da5fa1b7b4a1d24fb371b7bf6c6d0adc1
Marjorie Silva <Marjie@Marjories-MacBook-Pro.local>
2020-12-12 18:05:13 -0600

    saveGame.java

---
+++ src/com/company/saveGame.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class saveGame {
+    private Board board;
+    private Player whitePlayer;
+    private Player blackPlayer;
+    private ArrayList<Piece> pieces;
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public Player getWhitePlayer() {
+        return whitePlayer;
+    }
+
+    public Player getBlackPlayer() {
+        return blackPlayer;
+    }
+
+    public ArrayList<Piece> getPieces() {
+        return pieces;
+    }
+
+    public saveGame(Board board, Player whitePlayer, Player blackPlayer, ArrayList<Piece> pieces){
+        this.board = board;
+        this.whitePlayer = whitePlayer;
+        this.blackPlayer = blackPlayer;
+        this.pieces = pieces;
+    }
+}
