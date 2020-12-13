11fe8038417514a998d8a362163c011b99dd9a38
Marjorie Silva <Marjie@Marjories-MacBook-Pro.local>
2020-12-13 11:13:55 -0600

    gameSaver update 2

--- src/com/company/gameSaver.java
+++ src/com/company/gameSaver.java
@@ -7,7 +7,7 @@ public class gameSaver {
     public static void write(saveGame saveGame, Player player) {
         String fileName = player + LocalDateTime.now().toString() + ".xml";
         try {
-            XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(new FileOutputStream("fileName")));
+            XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(new FileOutputStream(fileName)));
             encoder.writeObject(saveGame);
             encoder.close();
         } catch (Exception ex) {
